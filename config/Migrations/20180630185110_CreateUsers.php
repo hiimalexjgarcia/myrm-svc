61d870ce3c3ac3f15074b9a306db75e5451fa572
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2018-06-30 11:56:01 -0700

    Added Users migration, model, controller and templates.

---
+++ config/Migrations/20180630185110_CreateUsers.php
@@ -0,0 +1,40 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+
+    public function up()
+    {
+
+        $this->table('users')
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+
+        $this->dropTable('users');
+    }
+}
+
